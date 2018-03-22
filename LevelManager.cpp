712a4b8cf76f19935bbd69a0c1b2f1a4a716e9b2
Gekkoinabox <36261451+Gekkoinabox@users.noreply.github.com>
2018-03-22 09:38:53 +0000

    Levelmanager.cpp edit

--- LevelManager.cpp
+++ LevelManager.cpp
@@ -93,4 +93,65 @@ int** LevelManager::nextLevel(VertexArray& rVaLevel)
 	//close the file
 	inputFile.close();
 
+	//What type of primitive are we using?
+	rVaLevel.setPrimitiveType(Quads);
+
+	//set size of vertex array
+	rVaLevel.resize(m_LevelSize.x * m_LevelSize.y * VERTS_IN_QUAD);
+
+	//Start at the beginning of the vertex array
+	int currentVertex = 0;
+
+	for (int x = 0;x < m_LevelSize.x; ++x)
+	{
+		for (int y = 0; y < m_LevelSize.y; ++y)
+		{
+			//position each vertex in the current quad
+			rVaLevel[currentVertex + 0].position = Vector2f(
+				x*TILE_SIZE, 
+				y*TILE_SIZE);
+
+			rVaLevel[currentVertex + 1].position = Vector2f(
+				x*TILE_SIZE + TILE_SIZE, 
+				y*TILE_SIZE);
+
+			rVaLevel[currentVertex + 2].position = Vector2f(
+				x*TILE_SIZE + TILE_SIZE,
+				y*TILE_SIZE + TILE_SIZE);
+
+			rVaLevel[currentVertex + 3].position = Vector2f(
+				x*TILE_SIZE, 
+				y*TILE_SIZE + TILE_SIZE);
+
+			//Which tile from the sprite sheet should we use
+			int verticalOffset = arrayLevel[y][x] * TILE_SIZE;
+
+			//set up texture coords
+			rVaLevel[currentVertex + 0].texCoords = Vector2f(
+				0,
+				0 + verticalOffset);
+
+			rVaLevel[currentVertex + 1].texCoords = Vector2f(
+				TILE_SIZE,
+				0 + verticalOffset);
+
+			rVaLevel[currentVertex + 2].texCoords = Vector2f(
+				TILE_SIZE,
+				TILE_SIZE + verticalOffset);
+
+			rVaLevel[currentVertex + 3].texCoords = Vector2f(
+				0,
+				TILE_SIZE + verticalOffset);
+
+			//Update our current vertex so we can draw the next quad
+			currentVertex = currentVertex + VERTS_IN_QUAD;
+		}//End y coord
+	}//end x coord
+	return arrayLevel;
 }//end function nextLevel()
+
+Vector2i LevelManager::getLevelSize()
+{
+
+}
+
